e3fa065005d4dc6ed768e3b6a0a92d11c28c5fd3
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-12-07 17:25:13 -0500

    In progress, but erroring still
    Can't see `trial_court.description`

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.ALAnyState',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=[],
+      install_requires=['docassemble.AssemblyLine'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ALAnyState/', package='docassemble.ALAnyState'),
      )
